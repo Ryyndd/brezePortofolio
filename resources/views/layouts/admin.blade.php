74e619179e7868101b21f8f423ec2cc66edfde31
Ryyndd <ferryfernando164@gmail.com>
2024-11-19 23:01:57 +0700

    admin logut

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -129,10 +129,20 @@
                 
 
                 <li>
-                    <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <i class="fi fi-bs-sign-out-alt text-xl font-semibold flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
-                        <span class="flex-1 ms-3 text-lg whitespace-nowrap">Logout</span>
-                    </a>
+                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <i class="fi fi-bs-sign-out-alt text-xl font-semibold flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
+                            <span class="flex-1 ms-3 text-lg whitespace-nowrap">
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <div :href="route('logout')"
+                                            onclick="event.preventDefault();
+                                                        this.closest('form').submit();">
+                                        {{ __('Logout') }}
+                                    </div>
+                                </form>
+                            </span>
+                        </a>
                 </li>
                 </ul>
             </div>
